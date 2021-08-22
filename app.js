9a2bde75b170cb35f316ad4b1f8278e54f48a3ac
Rajesh kumar <rk670141@gmail.com>
2021-08-22 10:34:03 +0530

    update

--- app.js
+++ app.js
@@ -164,9 +164,13 @@ app.post("/register", function (req, res) {
   })  
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
 
 
-
-app.listen("3000", function () {
-    console.log("server is running at port 3000");
+app.listen(port, function () {
+    console.log("server has started");
 })
